780c9bf646f29ef5858bb6de3f804d61a7ec9403
mbgoseco <mbgoseco@gmail.com>
2019-01-10 12:12:10 -0800

    initial setup

---
+++ Word_Guess_Game/Word_Guess_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Word_Guess_Game
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
