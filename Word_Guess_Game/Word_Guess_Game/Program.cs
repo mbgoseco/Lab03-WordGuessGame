a33c15b5cf5c94bd1b11f26640194ff3c7bdc1f1
mbgoseco <mbgoseco@gmail.com>
2019-01-11 12:14:45 -0800

    can add words to list

--- Word_Guess_Game/Word_Guess_Game/Program.cs
+++ Word_Guess_Game/Word_Guess_Game/Program.cs
@@ -33,26 +33,32 @@ namespace Word_Guess_Game
                 Console.WriteLine("<><><><><><><><><><><><><><><><><><>");
                 Console.Write("Choose a selection: ");
 
-                selection = Console.ReadLine();
-                switch (selection)
+                try
                 {
-                    case "1":
-                        NewGame(path);
-                        break;
-                    case "2":
-                        Options(path);
-                        break;
-                    case "3":
-                        Console.Clear();
-                        Console.WriteLine("Thank you for playing!");
-                        break;
-                    default:
-                        Console.Clear();
-                        throw new Exception("Please choose one of the given options.");
+                    selection = Console.ReadLine();
+                    switch (selection)
+                    {
+                        case "1":
+                            NewGame(path);
+                            break;
+                        case "2":
+                            Options(path);
+                            break;
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("Thank you for playing!");
+                            break;
+                        default:
+                            Console.Clear();
+                            throw new Exception("That selection does not exist.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    Console.WriteLine(e.Message);
                 }
             }
-
-            ShowWords(path);
         }
 
         /// <summary>
@@ -65,11 +71,11 @@ namespace Word_Guess_Game
             string[] words = { "dog", "unicorn", "mountain", "cheetah", "freeway", "destroy" };
             try
             {
-                using (StreamWriter wordFile = new StreamWriter(path))
+                using (StreamWriter streamWriter = new StreamWriter(path))
                 {
                     foreach (string word in words)
                     {
-                        wordFile.WriteLine(word);
+                        streamWriter.WriteLine(word);
                     }
                 }
             }
@@ -84,7 +90,86 @@ namespace Word_Guess_Game
 
         }
 
+        /// <summary>
+        /// Main options menu that allows user to view, add, or delete words.
+        /// </summary>
+        /// <param name="path"></param>
         public static void Options(string path)
+        {
+            Console.Clear();
+            string selection = "";
+            while (selection != "4")
+            {
+                Console.WriteLine("---------------OPTIONS--------------");
+                Console.WriteLine("<><><><><><><><><><><><><><><><><><>");
+                Console.WriteLine("1) View Words");
+                Console.WriteLine("2) Add Words");
+                Console.WriteLine("3) Delete Words");
+                Console.WriteLine("4) Main Menu");
+                Console.WriteLine("<><><><><><><><><><><><><><><><><><>");
+                Console.Write("Choose a selection: ");
+
+                try
+                {
+                    selection = Console.ReadLine();
+                    switch (selection)
+                    {
+                        case "1":
+                            Console.Clear();
+                            ShowWords(path);
+                            break;
+                        case "2":
+                            AddWords(path);
+                            break;
+                        case "3":
+                            DeleteWords(path);
+                            break;
+                        case "4":
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.Clear();
+                            throw new Exception("That selection does not exist.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public static void AddWords(string path)
+        {
+            try
+            {
+                using (StreamWriter streamWriter = File.AppendText(path))
+                {
+                    Console.Write("Enter a word to add to the list: ");
+                    string newWord = Console.ReadLine();
+                    if(newWord.Length > 0)
+                    {
+                        streamWriter.WriteLine(newWord);
+                        Console.Clear();
+                        Console.WriteLine($"{newWord} was added to the list.");
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        throw new Exception("No word was entered.");
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void DeleteWords(string path)
         {
 
         }
@@ -95,6 +180,7 @@ namespace Word_Guess_Game
         /// <param name="path"></param>
         public static void ShowWords(string path)
         {
+            Console.WriteLine("-----Word List-----");
             string[] words = File.ReadAllLines(path);
             foreach(string word in words)
             {
