eaab13eafdcdbcb55e9c5a438a205fe27bb8a916
mbgoseco <mbgoseco@gmail.com>
2019-01-12 14:48:40 -0800

    Tests added, screenshots taken

--- Word_Guess_Game/XUnitTestWordGuessGame/UnitTest1.cs
+++ Word_Guess_Game/XUnitTestWordGuessGame/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 using Word_Guess_Game;
 
@@ -6,10 +7,89 @@ namespace XUnitTestWordGuessGame
 {
     public class UnitTest1
     {
+        // Tests that a text file can be updated
         [Fact]
-        public void Test1()
+        public void CreateWordFile()
         {
+            string path = "../../../TestWords.txt";
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                streamWriter.WriteLine("one");
+                streamWriter.WriteLine("two");
+                streamWriter.WriteLine("three");
+            }
+            string[] words = File.ReadAllLines(path);
+            Assert.Contains(words[0], "one");
+            Assert.Contains(words[1], "two");
+            Assert.Contains(words[2], "three");
+        }
 
+        // Tests that a word can be added to the file
+        [Fact]
+        public void AddWords()
+        {
+            string path = "../../../TestWords.txt";
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                streamWriter.WriteLine("one");
+                streamWriter.WriteLine("two");
+                streamWriter.WriteLine("three");
+            }
+
+            using (StreamWriter streamWriter = File.AppendText(path))
+            {
+                streamWriter.WriteLine("four");
+                streamWriter.WriteLine("five");
+            }
+
+            string[] words = File.ReadAllLines(path);
+            Assert.Contains(words[3], "four");
+            Assert.Contains(words[4], "five");
+        }
+
+        // Test that all words can be retrieved from a file
+        [Fact]
+        public void ShowsAllWords()
+        {
+            string path = "../../../TestWords.txt";
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                streamWriter.WriteLine("cat");
+                streamWriter.WriteLine("dog");
+                streamWriter.WriteLine("rabbit");
+                streamWriter.WriteLine("turtle");
+                streamWriter.WriteLine("lizard");
+            }
+            string[] words = File.ReadAllLines(path);
+            string allWords = String.Join(", ", words);
+            Assert.Equal("cat, dog, rabbit, turtle, lizard", allWords);
+        }
+
+        // Tests to check if a guessed letter will reveal itself on the board or go to the pool of wrong letters as intended
+        [Fact]
+        public void LetterIsInWord()
+        {
+            char[] word = { 'w', 'i', 'n', 'n', 'e', 'r' };
+            char[] boardWord = { '_', '_', '_', '_', '_', '_' };
+            string wrongLetters = "";
+            string guess = "E";
+
+            Program.GuessLetter(word, boardWord, ref wrongLetters, guess);
+
+            Assert.Equal('E', boardWord[4]);
+        }
+        [Fact]
+        public void LetterNotInWord()
+        {
+            char[] word = { 'w', 'i', 'n', 'n', 'e', 'r' };
+            char[] boardWord = { '_', '_', '_', '_', '_', '_' };
+            string wrongLetters = "";
+            string guess = "x";
+
+            Program.GuessLetter(word, boardWord, ref wrongLetters, guess);
+
+            Assert.Equal('_', boardWord[5]);
+            Assert.Contains(wrongLetters, "X");
         }
     }
 }
